f6df652079773ddce747f9cdfc8b5e728aec719d
Joy Hauser <jhauser@viper.cs.ksu.edu>
2017-03-07 20:22:51 -0600

    Fix sys_open declaraction discrepancy error

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -12,7 +12,7 @@ pid_t sys_exec (const char *cmd_line);
 int sys_wait (pid_t pid);
 bool sys_create (const char *file, unsigned initial_size);
 bool sys_remove (const char *file);
-int sys_open (const char *file);
+static int sys_open (const char *file);
 int sys_filesize (int fd);
 int sys_read (int fd, void *buffer, unsigned size);
 int sys_write (int fd, const void *buffer, unsigned size);
