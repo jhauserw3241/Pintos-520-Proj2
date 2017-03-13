89d08d358314456ce54d9f6a84b2c8d1660800a2
Lauren Lynch <Lauren Lynch>
2017-03-13 00:45:37 -0500

    comments everywhere...

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -94,6 +94,9 @@ sys_halt(void) {
 static void
 sys_exit(int status) {
 	pid_t cur = getpid();
+	if (getppid() != NULL) {
+		//give parent status
+	}
 	// printf("%s: exit(%d)\n", cmd_line passed in process_exec, status);
 }
 
